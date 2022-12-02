d1fd7b373595ebd5d27b1969f6c3f60a40c8eb74
Your Name <you@example.com>
2022-12-02 16:16:31 +0200

    XD

---
+++ day1.py
@@ -0,0 +1,21 @@
+summa=[]
+with open('day1',mode="r") as input:
+    lista = input.read()
+    lista = lista.split("\n\n")
+    for x in range(len(lista)):
+        lista[x]=lista[x].split("\n")
+    for x in range(len(lista)):
+        temp=0
+        for x in lista[x]:
+            try:temp+=int(x)
+            except:pass
+        summa.append(temp)
+    print(max(summa))
+    for x in range(len(summa)):
+        if type(summa[x]) == None:
+            print("Hep")
+    summa.sort()
+    temp=0
+    for x in summa[-3:]:
+        temp+=x
+    print(temp)
